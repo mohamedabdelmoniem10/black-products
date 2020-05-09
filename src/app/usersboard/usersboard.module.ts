7ef40e1ccafea33b5ba6ddfe7234f7081aea3b59
Moniem Mohamed <m.moniem@fahmawy.com>
2020-05-09 19:21:51 +0200

    sat may 9 7:21

--- src/app/usersboard/usersboard.module.ts
+++ src/app/usersboard/usersboard.module.ts
@@ -1,10 +1,17 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { NavbarComponent } from './navbar/navbar.component';
+import { FooterComponent } from './footer/footer.component';
+import { HomeComponent } from './home/home.component';
+import { SigningComponent } from './signing/signing.component';
+import { ProductsComponent } from './products/products.component';
+import { CartComponent } from './cart/cart.component';
+import { ProfileComponent } from './profile/profile.component';
 
 
 
 @NgModule({
-  declarations: [],
+  declarations: [NavbarComponent, FooterComponent, HomeComponent, SigningComponent, ProductsComponent, CartComponent, ProfileComponent],
   imports: [
     CommonModule
   ]
